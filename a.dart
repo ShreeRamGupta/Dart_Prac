3f727d7f48abeaf4529cc89f6f0a8c5b0275055c
Shree Ram Gupta <49408824+ShreeRamGupta@users.noreply.github.com>
2021-03-08 14:47:32 +0530

    this is a example file of dart

---
+++ a.dart
@@ -0,0 +1,11 @@
+import 'dart:core';
+
+// void main(List<String> args) {
+//   print('Welcome, Dart!!');
+// }
+
+// void main() {
+//   print('Hello, Dart!!');
+// }
+
+void main() => print('Hello, Dart. :D');
